81d9573b0ebdb9d81605cf45279a2878ad91d2bd
amoldevakate <74484063+amoldevakate@users.noreply.github.com>
2026-01-05 11:39:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "Azodha-project" # Replace with your actual S3 bucket name
+    bucket = "azodha-project" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
